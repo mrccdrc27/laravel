d08a350a369adef180e706a23f4a62a69f4f5e2b
Ivo <rivovebayo@gmail.com>
2024-12-23 00:27:43 +0800

    MVC of LMS columns

--- lms-system/app/Http/Controllers/Auth/RegisteredUserController.php
+++ lms-system/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\UserInfo;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -30,17 +31,34 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            // 'name' => ['required', 'string', 'max:255'],
+            'FirstName' => ['required', 'string', 'max:50'],
+            'LastName' => ['required', 'string', 'max:50'],
+            'BirthDate' => ['required', 'date', 'before_or_equal:today'],
+            'Sex' => ['required', 'boolean'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            // 'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
+
+        // Save informations in the users_info table
+        UserInfo::create([
+        'UserID' => $user->id, // Associate with the user
+        'FirstName' => $request->FirstName,
+        'MiddleName' => $request->MiddleName,
+        'LastName' => $request->LastName,
+        'BirthDate' => $request->BirthDate,
+        'Nationality'=> $request->Nationality,
+        'BirthPlace'=> $request->BirthPlace,
+        'Sex' => $request->Sex,
+        'Role' => $request->role ?? 'student',
+        ]);
+
+        
         event(new Registered($user));
 
         Auth::login($user);
--- lms-system/resources/views/auth/register.blade.php
+++ lms-system/resources/views/auth/register.blade.php
@@ -2,13 +2,68 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
-        <!-- <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div> -->
+        <!-- First Name -->
+        <div>
+            <x-input-label for="First Name" :value="__('First Name')" />
+            <x-text-input id="FirstName" class="block mt-1 w-full" type="text" name="FirstName" :value="old('FirstName')" required autofocus autocomplete="FirstName" />
+            <x-input-error :messages="$errors->get('FirstName')" class="mt-2" />
+        </div>
 
+        <!-- Middle Name -->
+        <div>
+            <x-input-label for="Middle Name" :value="__('Middle Name')" />
+            <x-text-input id="MiddleName" class="block mt-1 w-full" type="text" name="MiddleName" :value="old('MiddleName')" />
+            <!-- <x-input-error :messages="$errors->get('MiddleName')" class="mt-2" /> -->
+        </div>
+
+        <!-- Last Name -->
+        <div>
+            <x-input-label for="Last Name" :value="__('Last Name')" />
+            <x-text-input id="LastName" class="block mt-1 w-full" type="text" name="LastName" :value="old('LastName')" required autofocus autocomplete="LastName"/>
+            <x-input-error :messages="$errors->get('LastName')" class="mt-2" />
+        </div>
+
+        <!-- Nationality -->
+        <div>
+            <x-input-label for="Nationality" :value="__('Nationality')" />
+            <x-text-input id="Nationality" class="block mt-1 w-full" type="text" name="Nationality" :value="old('Nationality')" required autofocus autocomplete="Nationality"/>
+            <x-input-error :messages="$errors->get('Nationality')" class="mt-2" />
+        </div>
+
+        <!-- Birth Place -->
+        <div>
+            <x-input-label for="BirthPlace" :value="__('BirthPlace')" />
+            <x-text-input id="BirthPlace" class="block mt-1 w-full" type="text" name="BirthPlace" :value="old('BirthPlace')" required autofocus autocomplete="BirthPlace"/>
+            <x-input-error :messages="$errors->get('BirthPlace')" class="mt-2" />
+        </div>
+
+        <!-- Birthdate -->
+        <div>
+            <x-input-label for="Birth Date" :value="__('Birth Date')" />
+            <x-text-input id="BirthDate" class="block mt-1 w-full" type="date" name="BirthDate" :value="old('BirthDate')" required autofocus autocomplete="BirthDate" max="{{ now()->toDateString() }}" />
+            <x-input-error :messages="$errors->get('Birthdate')" class="mt-2" />
+        </div>
+
+
+        <!-- Sex -->
+        <div>
+        <x-input-label for="Sex" :value="__('Sex')" />
+        <div class="flex items-center space-x-4 mt-1">
+            <!-- Male -->
+            <label class="flex items-center">
+                <input type="radio" id="SexMale" name="Sex" value="1" class="form-radio" required>
+                <span class="ml-2">{{ __('Male') }}</span>
+            </label>
+            <!-- Female -->
+            <label class="flex items-center">
+                <input type="radio" id="SexFemale" name="Sex" value="0" class="form-radio">
+                <span class="ml-2">{{ __('Female') }}</span>
+            </label>
+        </div>
+        <x-input-error :messages="$errors->get('Sex')" class="mt-2" />
+        </div>
+
+        
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
