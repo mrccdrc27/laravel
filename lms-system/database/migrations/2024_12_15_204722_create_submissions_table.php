39560d576ff164f410c74fd418052f211fd4afcd
mrccdrc27 <cubecore27@gmail.com>
2025-01-10 20:15:46 +0800

    migration update

--- lms-system/database/migrations/2024_12_15_204722_create_submissions_table.php
+++ lms-system/database/migrations/2024_12_15_204722_create_submissions_table.php
@@ -12,18 +12,23 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('submissions', function (Blueprint $table) {
-            $table->id('submissionID'); // Primary key
-            $table->unsignedBigInteger('assignmentID')->nullable();
-            $table->unsignedBigInteger('facultyID');
-            $table->unsignedBigInteger('studentID');
-            $table->text('content')->nullable();
-            $table->string('fileName', 255)->nullable(false);
-            $table->string('fileType', 50)->nullable(false);
-            $table->binary('fileData')->nullable(false); 
-            $table->timestamp('submittedAt')->useCurrent();
-            $table->float('grade')->nullable();
+            $table->id('SubmissionID'); // Primary key
+            //$table->unsignedBigInteger('AssignmentID')->nullable();
+            //$table->unsignedBigInteger('AssessmentID')->nullable();
+            $table->unsignedBigInteger('FacultyID');
+            $table->unsignedBigInteger('StudentID');
+            $table->text('Content')->nullable();
+            $table->string('FileName', 255)->nullable(false);
+            $table->string('FileType', 50)->nullable(false);
+            $table->binary('FileData')->nullable(false); 
+            $table->timestamp('SubmittedAt')->useCurrent();
+            $table->float('Grade')->nullable();
         
             // Foreign keys
+            $table->foreign('AssignmentID')->references('AssignmentID')->on('assignments')->onDelete('no action');
+        //$table->foreign('AssessmentID')->references('AssessmentID')->on('assessments')->onDelete('no action');
+        $table->foreign('FacultyID')->references('id')->on('users')->onDelete('no action');
+        $table->foreign('StudentID')->references('id')->on('users')->onDelete('no action');
             $table->foreign('assignmentID')->references('assignmentID')->on('assignments')->onDelete('no action');
         $table->foreign('facultyID')->references('id')->on('users')->onDelete('no action');
         $table->foreign('studentID')->references('id')->on('users')->onDelete('no action');
