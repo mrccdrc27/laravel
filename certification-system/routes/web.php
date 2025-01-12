41b8a59e45ca6f7fcc53711bf3f25da22e182661
mrccdrc27 <cubecore27@gmail.com>
2025-01-12 21:35:36 +0800

    blade layouts

--- certification-system/routes/web.php
+++ certification-system/routes/web.php
@@ -4,6 +4,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\CertificationController;
 
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('certificate', function () {
+    return view('dashboard.certificate');
+})->name('certificate');
+
+Route::get('home', function () {
+    return view('dashboard.home');
+})->name('home');
+
+Route::get('/', function () {
+    return redirect()->route('home');
+});
+
+Route::get('about', function () {
+    return view('dashboard.about');
+})->name('about');
+
+Route::get('search', function () {
+    return view('dashboard.search');
+})->name('search');
