f9084b3a6fc41fc97644e58b285394e3c2f8ca89
Ivo <rivovebayo@gmail.com>
2025-01-16 18:54:02 +0800

    LAST NA TO PRAMIS

--- resources/views/admin/dashboard.blade.php
+++
@@ -1,3 +0,0 @@
-<h6>Hello Admin!</h6>
-<h1> HEloo Admin Pakyu ka din!!!!! </h1>
-    
--- resources/views/student/dashboard.blade.php
+++
@@ -1,6 +0,0 @@
-@role('student')
-        <p>Hello Student!.</p>
-    <h1>Hello STUDENT!</h1>
-    @endrole
-
-    <p>Pakyu kayong lahat!.</p>
