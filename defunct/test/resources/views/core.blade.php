62380d0e55beca1662f39b20baef443c708ec0d3
mrccdrc27 <cubecore27@gmail.com>
2025-01-22 02:38:46 +0800

    RBAC -> main

--- defunct/test/resources/views/core.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- resources/views/toys.blade.php -->
-<h1>core List</h1>
-<ul>
-    @foreach($core as $cor)
-        <li>{{ $cor->model }} - ${{ $cor->manufacturer }}</li>
-    @endforeach
-</ul>
