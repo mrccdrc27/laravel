7803692f128576282e5697933d4e5391cbb7aa2b
adi <125032843+lucky7fever@users.noreply.github.com>
2025-01-28 22:17:41 +0800

    Revert "- merging commit"
    This reverts commit bc293068e96d87d93d789aed91b8265e11457a64.

--- certification-system/storage/framework/views/03bb1233aa87f9715a4f0e3a4e3c6ff0.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <div class="alert alert-danger">
-        <h4 class="alert-heading">Something Went Wrong</h4>
-        <p>We're sorry, but an error occurred. Please try again later.</p>
-        <a href="<?php echo e(route('home')); ?>" class="btn btn-primary">Return to Home</a>
-    </div>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layout.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Bentulan\source\repos\laravel\certification-system\resources\views/errors/generic_error.blade.php ENDPATH**/ ?>
