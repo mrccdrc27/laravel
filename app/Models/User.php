2490196e4bbd1a4587e738ee34271261e5f23fb8
adi <125032843+lucky7fever@users.noreply.github.com>
2025-01-29 22:47:17 +0800

    - Fixed route in cert view

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,33 +1,21 @@
 <?php
+
 namespace App\Models;
 
+use App\View\Components\Userinfo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory;
 
-    // Define the table if it's different from the default (in this case, user_info table)
-    protected $table = 'user_info';
+    protected $connection = 'sqlsrv_lms';
+    protected $table = 'users';
+    protected $primaryKey = 'id';
 
-    protected $primaryKey = 'userID';
-
-    // Define fillable attributes (for mass assignment)
-    protected $fillable = [
-        'role',
-        'username',
-        'email',
-        'password',
-        'is_active',
-        'last_login_at',
-    ];
-
-    // If using timestamps for 'created_at' and 'updated_at'
-    public $timestamps = true;
-
-    // If you're using soft deletes
-    protected $dates = ['deleted_at', 'last_login_at'];
-
-
+    public function info()
+    {
+        return $this->hasOne(Userinfo::class, 'userID', 'id');
+    }
 }
